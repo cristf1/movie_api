5df8724339efa0e099a2c15ab0f198011c29ae1f
cristf1 <cristf1@users.noreply.github.com>
2022-09-02 16:34:27 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+      url=require('url');
+      fs = require('fs');
+
+
+http.createServer((request, response) => {
+
+let addr = request.url;
+let q = url.parse(addr,true);
+filePath = '';
+
+console.log(addr);
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+
+if (q.pathname.includes('documentation')) {
+   filePath = (__dirname + '/documentation.html');
+ } else {
+   filePath = 'index.html';
+ };
+
+
+ fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+   if (err) {
+     console.log(err);
+   } else {
+     console.log('Added to log.');
+   };
+ });
+
+
+response.writeHead(200, {'Content-Type': 'text/plain'});
+response.end('Hello Node!\n');
+}).listen(8080);
+console.log('My test server is running on Port 8080.');
---
+++ test.js
@@ -0,0 +1,9 @@
+console.log('Goodbye.');
+
+import { Buffer } from 'buffer';
+
+const bufs = Buffer.from([1, 2, 3, 4]);
+
+for (const buf of bufs) {
+  console.log(buf);
+}
