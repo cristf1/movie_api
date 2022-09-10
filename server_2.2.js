ff75b0069e29ba1ae7f90f9a546888b5527fa583
cristf1 <cristf1@users.noreply.github.com>
2022-09-10 14:48:12 -0700

    Update to read documentation

--- server_2.2.js
+++ server_2.2.js
@@ -31,6 +31,16 @@ if (q.pathname.includes('documentation')) {
 
 
 response.writeHead(200, {'Content-Type': 'text/plain'});
+fs.readFile(filePath, (err, data) => {
+     if (err) {
+       throw err;
+     }
+
+     response.writeHead(200, { 'Content-Type': 'text/html' });
+     response.write(data);
+     response.end();
+   });
+
 response.end('Hello Node!\n');
 }).listen(8080);
 console.log('My test server is running on Port 8080.');
