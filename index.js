083bb9e0183205fadfdce7b693875302c9f05b6d
cristf1 <cristf1@users.noreply.github.com>
2022-10-11 15:25:46 -0700

    Update for Task 2.9 on 10/11

--- index.js
+++ index.js
@@ -17,87 +17,19 @@ const app = express();
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
+const cors = require('cors');
+app.use(cors());
+
  let auth = require('./auth')(app);
  const passport = require('passport');
 require('./passport');
-
-
-let top10movies = [
-  {name:"The Rescue",
-genre: {name:'documentary'},
-director:{name:['Jimmy Chin', 'Elizabeth Chai Vasarhelyi']} },
-  {name:"Raya",
-  genre:{name:'family film'},
-  director: {name:['Carlos López Estrada', 'Don Hall']}},
-  {name:"Frozen",
-genre:{name:'family film'},
-director: {name:['Chris Buck', 'Jennifer Lee'] } },
-  {name:"Frozen II",
-genre: {name:'family film'},
-director: {name:['Chris Buck', 'Jennifer Lee']}},
-  {name:"Little Mermaid",
-genre: {name:'family film'},
-director:{name: ['John Musker', 'Ron Clements']} },
-  {name:"Karate Kid",
-genre:{name:'martial arts'},
-director:{name:'Harald Zwart'}},
-  {name:"Secretly, Greatly",
-genre: {name:'comedy-drama'},
-director: {name:'Jang Cheol-soo'}},
-  {name:"ShangChi",
-genre: {name:'marvel'},
-director: {name:'Destin Daniel Cretton'}},
-  {name:"Spider-Man: No Way Home",
-genre:{name:'action'},
-director: {name:'Jon Watts'}},
-  {name:"Avengers: Endgame",
-genre: {name:'action'},
-director:{name:["Anthony Russo", "Joe Russo"]}},
-];
-
-let users= [
-  {id: 1,
-    name: "Cristine Fang",
-    username: "cristinefang",
-    favoritemovie:["The Rescue"],
-    email: "cristinefang@gmail.com"
-  },
-  {id: 2,
-    name: "Wallace Fang",
-    username: "wallacefang",
-    favoritemovie:["Karate Kid"],
-    email: "wallacefangfang@gmail.com"
-  }
-];
+x
 
 app.get("/", (req,res) => {
   res.send('Welcome to MyFlix');
 });
 
 app.use(express.static('public'));
-
-app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) =>{
-  Movies.find()
-  .then((movies) => {
-    res.status(201).json(movies);
-  })
-  .catch((error)=>{
-    console.error(error);
-    res.status(500).send('Error:' + error);
-  });
-});
-
-
-//Add a user
-/* We’ll expect JSON in this format
-{
-  ID: Integer,
-  Username: String,
-  Password: String,
-  Email: String,
-  Birthday: Date
-}*/
-
 
 app.post('/users', (req, res) => {
   Users.findOne({ Username: req.body.Username })
@@ -127,7 +59,7 @@ app.post('/users', (req, res) => {
 
 
 //Return Users Collection
-app.get('/users', (req,res) => {
+app.get('/users', passport.authenticate('jwt', {session: false}), (req,res) => {
   Users.find()
   .then((users) => {
     res.status(201).json(users);
@@ -139,7 +71,7 @@ app.get('/users', (req,res) => {
 });
 
 //Return specific user
-app.get('/users/:Username', (req,res)=> {
+app.get('/users/:Username', passport.authenticate('jwt', {session: false}), (req,res)=> {
   Users.findOne({Username: req.params.Username})
   .then((user)=> {res.json(user)})
   .catch((err)=> {
@@ -147,18 +79,8 @@ app.get('/users/:Username', (req,res)=> {
   })
 });
 
-// Update a user's info, by username
-/* We’ll expect JSON in this format
-{
-  Username: String,
-  (required)
-  Password: String,
-  (required)
-  Email: String,
-  (required)
-  Birthday: Date
-}*/
-app.put('/users/:Username', (req, res) => {
+
+app.put('/users/:Username', passport.authenticate('jwt', {session: false}), (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
       Username: req.body.Username,
@@ -179,7 +101,7 @@ app.put('/users/:Username', (req, res) => {
 });
 
 //Add a movie to user's list of favorite
-app.post('/users/:Username/movies/:MovieID', (req, res) => {
+app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', {session: false}), (req, res) => {
   Users.findOneAndUpdate({Username:req.params.Username},{$push:
   {FavoriteMovies: req.params.MovieID}
 },
@@ -195,7 +117,7 @@ app.post('/users/:Username/movies/:MovieID', (req, res) => {
 });
 
 //Remove a movie from user's list of favorite
-app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', {session: false}), (req, res) => {
   Users.findOneAndUpdate({Username:req.params.Username},{$pull:
   {FavoriteMovies: req.params.MovieID}
 },
@@ -213,7 +135,7 @@ app.delete('/users/:Username/movies/:MovieID', (req, res) => {
 
 
 // Delete a user by username
-app.delete('/users/:Username', (req, res) => {
+app.delete('/users/:Username', passport.authenticate('jwt', {session: false}), (req, res) => {
   Users.findOneAndRemove({ Username: req.params.Username })
     .then((user) => {
       if (!user) {
@@ -231,7 +153,7 @@ app.delete('/users/:Username', (req, res) => {
 //MOVIES
 
 //Return a list of all movies to the user
-app.get('/movies', (req, res)=>{
+app.get('/movies', passport.authenticate('jwt', {session: false}),(req, res)=>{
   Movies.find()
   .then((movie)=>{
     res.status(201).json(movie)
@@ -243,7 +165,7 @@ app.get('/movies', (req, res)=>{
 });
 
 //Return data about a single movie by title
-app.get('/movies/:Title', (req, res)=>{
+app.get('/movies/:Title', passport.authenticate('jwt', {session: false}), (req, res)=>{
   Movies.findOne({Title: req.params.Title})
   .then((movie) => {res.json(movie)})
   .catch((err) => {
@@ -254,7 +176,7 @@ app.get('/movies/:Title', (req, res)=>{
 
 
 //Return data about genre by genre name
-app.get("/movies/genre/:genreName", (req,res)=>{
+app.get("/movies/genre/:genreName", passport.authenticate('jwt', {session: false}), (req,res)=>{
   Movies.find({'Genre.Name':req.params.genreName}) //not sure about this naming convention
   .then((movie) => {
     res.status(201).json(movie)
@@ -268,7 +190,7 @@ app.get("/movies/genre/:genreName", (req,res)=>{
 
 
 //Return data about a director by name
-app.get("/movies/director/:directorName", (req,res)=>{
+app.get("/movies/director/:directorName", passport.authenticate('jwt', {session: false}), (req,res)=>{
   Movies.find({"Director.Name":req.params.directorName})
   .then((movie) => {
     res.json(movie)
