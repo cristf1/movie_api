3e488367dbced64fd54f8912f57913ea54f5da7a
cristf1 <cristf1@users.noreply.github.com>
2022-10-12 16:35:10 -0700

    Updated for Task 2.10

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const express = require('express');
  const Movies = Models.Movie;
  const Users = Models.User;
 
- mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+ongoose.connect( process.env.CONNECTIOIN_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+ //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
