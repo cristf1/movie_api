9079892b9a9184ce8e04f8b1cf4c5279751557fe
cristf1 <cristf1@users.noreply.github.com>
2022-11-17 15:56:58 -0800

    Updated authentication

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 //MOVIES
 
 //Return a list of all movies to the user
-app.get('/movies', /*passport.authenticate('jwt', {session: false}),*/(req, res) => {
+app.get('/movies', passport.authenticate('jwt', {session: false}),(req, res) => {
   Movies.find()
     .then((movie) => {
       res.status(201).json(movie)
